baa14aa106e63e4b6efdf0c30e938ece4c6e0424
Cyrick Kyle Tapay <tapaycyrickkyle@gmail.com>
2026-07-16 08:05:17 +0800

    reorder gallery: newest images first (reverse chronological)

--- app/components/Gallery.tsx
+++ app/components/Gallery.tsx
@@ -3,16 +3,16 @@ import Image from "next/image";
 import ScrollAnimation from "./ScrollAnimation";
 
 const IMAGES = [
-  { src: "/images/gallery/gallery-1.jpg", alt: "Project photo 1", span: "md:col-span-2 md:row-span-2" },
-  { src: "/images/gallery/gallery-2.jpg", alt: "Project photo 2", span: "" },
-  { src: "/images/gallery/gallery-3.jpg", alt: "Project photo 3", span: "" },
-  { src: "/images/gallery/gallery-4.jpg", alt: "Project photo 4", span: "" },
-  { src: "/images/gallery/gallery-5.jpg", alt: "Project photo 5", span: "" },
-  { src: "/images/gallery/gallery-6.jpg", alt: "Project photo 6", span: "" },
-  { src: "/images/gallery/gallery-7.jpg", alt: "Project photo 7", span: "md:col-span-2" },
-  { src: "/images/gallery/gallery-8.jpg", alt: "Project photo 8", span: "" },
-  { src: "/images/gallery/gallery-9.jpg", alt: "Project photo 9", span: "" },
-  { src: "/images/gallery/gallery-10.jpg", alt: "Project photo 10", span: "" },
+  { src: "/images/gallery/b35af487-d751-452a-be98-2ff8420c542b.jpg", alt: "Project photo 1", span: "md:col-span-2 md:row-span-2" },
+  { src: "/images/gallery/83af6d4e-6d88-4fff-92d9-22e937331dd5.jpg", alt: "Project photo 2", span: "" },
+  { src: "/images/gallery/cb4e714f-3aab-42dd-a1b3-009724575091.jpg", alt: "Project photo 3", span: "" },
+  { src: "/images/gallery/73473f6d-8598-4743-8e04-86d062a0d2f2.jpg", alt: "Project photo 4", span: "" },
+  { src: "/images/gallery/f67fa57a-ba6e-466d-afcf-942ca1251623.jpg", alt: "Project photo 5", span: "" },
+  { src: "/images/gallery/426ed81d-221a-489e-9161-3f03e507112a.jpg", alt: "Project photo 6", span: "" },
+  { src: "/images/gallery/bfc57ffa-db43-4a80-a049-b412ee22943e.jpg", alt: "Project photo 7", span: "md:col-span-2" },
+  { src: "/images/gallery/2e9bcaad-06ae-4b8d-8f58-bfea6f7486c3.jpg", alt: "Project photo 8", span: "" },
+  { src: "/images/gallery/11569caf-84c4-417c-92b9-d182e2218bce.jpg", alt: "Project photo 9", span: "" },
+  { src: "/images/gallery/392bf38a-ea2f-411e-81de-1826022936ec.jpg", alt: "Project photo 10", span: "" },
 ];
 
 const VIDEOS_FEATURED = [
